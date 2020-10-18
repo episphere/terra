3f9a87475a99199e8d70e5da226840a68a7378cf
Jonas Almeida <jalmeida@mathbiol.org>
2020-10-18 17:54:23 -0400

    pkg v1

--- background.js
+++
@@ -1,1 +0,0 @@
-alert('hello world at '+Date())
--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,8 @@
 console.log('epiTerra.js ...')
 setTimeout(function(){
     epiTerraScript = document.createElement('script')
-    epiTerraScript.src='http://localhost:8000/terra/epiTerra.js'
+    //epiTerraScript.src='http://localhost:8000/terra/epiTerra.js'
+    epiTerraScript.src='https://episphere.github.io/terra/epiTerra.js'
     console.log('... appended at '+Date()+' by a content script ...')
     document.head.appendChild(epiTerraScript)
 },3000)
